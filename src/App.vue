2c87bd03e62dc96b76a58463ae9e186f3e7a1e04
Nothingspecial2468 <167680098+Nothingspecial2468@users.noreply.github.com>
2026-01-27 18:49:37 +0530

    Make things alive

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,28 @@
 <script setup>
-import Header from "./components/Header.vue";
-import MainContent from "./components/MainContent.vue";
-import Footer from "./components/Footer.vue";
-
 import { ref } from "vue";
 const count = ref(0);
+const newTodo = ref("");
 </script>
 
 <template>
-  <Header />
+  <h1>Vue Playground</h1>
+
   <p>
-    Let's add some fun. Click it as many times as u want so the click is
+    Click as many times you want:
     <button @click="count++">
       {{ count }}
     </button>
-    times clicked.
   </p>
-  <MainContent />
-  <Footer />
+
+  <h2>Todo App</h2>
+  <input type="text" placeholder="Type a todo...." v-model="newTodo" />
+  <p>You typed: {{ newTodo }}</p>
 </template>
 
 <style scoped>
-p {
-  padding: 2rem;
-  text-align: center;
-  font-size: large;
-  font-weight: bold;
-  margin-top: 70px;
-  background: #333;
-}
 button {
   cursor: pointer;
-  margin: 10px;
+  margin-left: 7px;
   border: 1px solid black;
   border-radius: 3px;
 }
