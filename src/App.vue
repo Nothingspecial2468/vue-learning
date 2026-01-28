081e8543faa686efed79ffb0aa539f41e42c7034
Neha <167680098+Nothingspecial2468@users.noreply.github.com>
2026-01-28 09:55:16 +0530

    Build reactive ToDo app with Vue state and directives

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,106 @@
 <script setup>
 import { ref } from "vue";
-const count = ref(0);
 const newTodo = ref("");
+const todos = ref([]);
+
+function addTodo(){
+  const value = newTodo.value.trim();
+  if(!value) return;
+
+  todos.value.push({
+    id: Date.now(),
+    text: value,
+    done: false
+  });
+
+  newTodo.value = "";
+}
 </script>
 
 <template>
-  <h1>Vue Playground</h1>
 
-  <p>
-    Click as many times you want:
-    <button @click="count++">
-      {{ count }}
-    </button>
-  </p>
+  <h1>Todo App</h1>
+  <input 
+  type="text" 
+  placeholder="Type a todo...." 
+  v-model="newTodo" 
+  @keyup.enter="addTodo" 
+  />
 
-  <h2>Todo App</h2>
-  <input type="text" placeholder="Type a todo...." v-model="newTodo" />
   <p>You typed: {{ newTodo }}</p>
+
+  <button @click="addTodo">
+    Add Todo
+  </button>
+
+  <ul>
+    <p class="list-title">Here is the list of todos:</p>
+    <li v-for="todo in todos" :key="todo.id">
+      <input type="checkbox" v-model="todo.done">
+      <span :class="{ done: todo.done}">
+        {{ todo.text }}
+      </span>
+    </li>
+  </ul>
+
+  <!-- <pre>{{ todos }}</pre> -->
 </template>
 
 <style scoped>
-button {
+h1{
+  color: cornflowerblue;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  text-align: center;
+  gap: 10px;
+}
+
+input[type="text"]{
+  padding: 15px;
+  font-size: 14px;
+  width:300px;
+  margin: 10px auto;
+  display: block;
+}
+
+p{
+  text-align: center;
+  font-size: medium;
+  text-decoration-thickness: 8px;
+}
+
+button{
+  padding: 10px 15px;
+  font-size: 14px;
   cursor: pointer;
-  margin-left: 7px;
-  border: 1px solid black;
-  border-radius: 3px;
+  background-color: cornflowerblue;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  margin-bottom: 20px;
+  display: block;
+  margin: 10px auto;
+}
+
+ul{
+  list-style-type: none;
+  padding: 0;
+  max-width: 400px;
+  margin: 20px auto;
+}
+
+.list-title{
+  font-weight: bold;
+  text-align: center;
+}
+
+li{
+  font-size: medium;
+  margin: 10px 0;
+}
+
+.done{
+  text-decoration: line-through;
+  color: gray;  
+  opacity: 0.6;
 }
 </style>
